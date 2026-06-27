772bfb754aee3989a9951e6b854e7d352107ff00
James Chu <marchuqq@gmail.com>
2026-06-27 11:27:02 +0800

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SCORING_CONFIG = {
   googleClientId: "571989823373-8jd1mupg52ieulumj35t5u1vu73c2bli.apps.googleusercontent.com",
-  spreadsheetId: "1pgNBbcZapwRVIDYrZeSaaGCs5mWpB-e9",
+  spreadsheetId: "1CgVeBD55VmhFYgDHpDux5JHT6VwpOI_oLfK1mU8Q3bI",
   appTitle: "2026 MIP挑戰營計分系統",
   refreshIntervalMs: 30000
 };
